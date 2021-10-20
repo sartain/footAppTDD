098ee6acdf67f0fcab1da823044af67db0b57e88
sartain <alex.sartain@zuhlke.com>
2021-10-20 09:17:35 +0100

    Update JSON file test\n\n Ensure all elements from file are written

--- src/__test__/PlayerSelection.spec.js
+++ src/__test__/PlayerSelection.spec.js
@@ -1,12 +1,16 @@
 import PlayerSelection from "../PlayerSelection";
 import { render } from "@testing-library/react";
+import playerList from "../playerList.json";
 //We want to load the players
-describe("Player Selection", () => {
+describe("Player Selection loading from JSON", () => {
   const playerFromFile = "pickford";
   it("Loads players from a json file", () => {
-    const { container, getByText } = render(<PlayerSelection />);
-    expect(getByText(playerFromFile)).toBeInTheDocument();
+    let { container, getByText } = render(<PlayerSelection />);
+    for (let playerID = 0; playerID < playerList.length; playerID++) {
+      expect(getByText(playerList[playerID].name)).toBeInTheDocument();
+    }
   });
-  it("Puts players into a list", () => {});
-  it("Displays the list on a screen", () => {});
+  it("Displays the players on the screen", () => {});
 });
+//Possibly all implicit? If the list exists then displayed on screen can check individual elements
+//E.g checking the state even though the values are not stored in state
